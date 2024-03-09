5fde8b0a0364bc1dbdf773c060568b7e66fa6df2
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-09 17:38:31 +0000

    Responsive Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
   "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-  extend: {},
+    
+  extend: {
+    colors:{
+      primary: "#1565D8",
+      dark: {
+        hard: "#0D2436",
+        soft: "#183B56",
+      },
+    },
+    fontFamily: {
+      opensans: ["Open Sans", "sans-serif"], 
+      roboto: ["Roboto", "sans-serif"]
+       }, 
+    },
   },
   plugins: [],
-  }
+};
   
