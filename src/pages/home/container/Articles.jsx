1e89d0f556c6c74a36b29db0203c87ca6641559e
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-16 09:07:42 +0000

    Article card component

---
+++ src/pages/home/container/Articles.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import ArticleCard from "../../../components /ArticleCard"
+
+const Articles = () => {
+
+    return (
+        <section className="container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5  py-10">
+            <ArticleCard className="w-full"/>
+        </section>
+  )
+}
+
+export default Articles
