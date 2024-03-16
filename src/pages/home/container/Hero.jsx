416131c4d34346bde419445b34b8ef23951c6759
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-16 07:59:47 +0000

    Hero Section

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -18,23 +18,23 @@ const Hero = () => {
         <div className='flex flex-col gap-y-2.5 mt-10 relative'>
           <div className='relative'>
             <TfiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]' />
-            <input className='placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none' 
+            <input className='placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4' 
             type='text' 
             placeholder='Search article'/>
           </div>
-          <button>Search</button>
+          <button className='w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2'>Search</button>
         </div>
-        <div>
-          <span>Popular Tags:</span>
-          <ul>
-            <li>Design</li>
-            <li>User Experience</li>
-            <li>User Interface</li>
+        <div className='flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7'>
+          <span className='text-dark-light font-semibold italic mt-2 lg:mt-4'>Popular Tags:</span>
+          <ul className='flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3'>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>Design</li>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Experience</li>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Interface</li>
           </ul>
         </div>
       </div>
-      <div className='hidden'>
-        <img src={images.HeroImage} alt="users are reading articles" />
+      <div className='hidden lg:block lg:1/2'>
+        <img className="w-full" src={images.HeroImage} alt="users are reading articles" />
       </div>
     </section>
   );
