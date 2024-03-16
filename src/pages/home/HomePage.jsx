c9849c297af12553fbf73474bcdf94da0324c640
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-16 19:44:12 +0000

    CTA section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainLayout from "../../components /MainLayout";
 import Hero from "./container/Hero";
 import Articles from "./container/Articles";
+import CTA from "./container/CTA";
 
 export const HomePage = () => {
   return (
--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -4,7 +4,8 @@ const CTA = () => {
   return (
     <>
     <svg  
-    className='w-full h-auto max-h-40'
+    className='w-full h-auto max-h-40 translate-y-[1px]'
+    preserveAspectRatio="none"
     viewBox="0 0 2160 263" 
     fill="none" 
     xmlns="http://www.w3.org/2000/svg">
@@ -17,7 +18,17 @@ const CTA = () => {
      />
     </svg>
 
-     <section></section>
+     <section className='relative bg-dark-hard px-5'>
+        <div className='container grid grid-cols-12 mx-auto'>
+          <div className='col-span-12'>
+            <h2 className='text-white font-roboto font-bold text-2xl'>Get our stories delivered From us to your inbox weekly.</h2>
+            <div className='w-full max-w-[494px] mt-12 space-y-3 mx-auto'>
+                <input type="text" className='px-4 py-3 rounded-lg w-full placeholder:text-dark-light' placeholder='Your Email' />
+                <button>Get started</button>
+            </div>
+          </div>
+        </div>
+     </section>
     </>
   )
 }
