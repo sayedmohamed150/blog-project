61b804fae81b4e246b2cd77cfb8467873106444d
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-17 06:15:12 +0000

    CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,6 +2,8 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
+
 
 
 const images = {
@@ -9,6 +11,7 @@ const images = {
     HeroImage,
     Post1Image,
     PostProfileImage,
+    CtaImage,
 };
 
 export default images;
--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -42,23 +42,6 @@ const CTA = () => {
                 <p className="text-dark-light mt-3 text-sm md:text-lg">
                 Majority of peole will work in jobs that don’t exist today.
                 </p>
-                <div className="flex justify-between flex-nowrap items-center mt-6">
-                    <div className="flex items-center gap-x-2 md:gap-x-2.5">
-                        <img src={images.PostProfileImage} alt="post profile" className="w-9 h-9 md:w-10 md:h-10" />
-                        <div className="flex flex-col">
-                            <h4 className="font-bold italic text-dark-soft text-sm md:text-base">Viola Manisa</h4>
-                            <div className="flex items-center gap-x-2">
-                                <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
-                                    <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
-                                    </span>
-                                <span className="italic text-dark-light text-xs md:text-sm">
-                                Verified writer
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <span className="font-bold text-dark-light italic text-sm md:text-base">02 May</span>
-                </div>
             </div>
         </div>
           </div>
