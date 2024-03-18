951d5f96921019597c1e624009c68a5e52867cee
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-18 08:39:18 +0000

    Footer

--- src/components /Footer.jsx
+++ src/components /Footer.jsx
@@ -2,7 +2,92 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='bg-dark-hard'>
+      <footer className='container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5'>
+
+        <div className='col-span-5'>
+          <h3 className='text-dark-light font-bold'>Product</h3>
+          <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+            <li>
+              <a href='/'>Landingpage</a>
+            </li>
+            <li>
+              <a href='/'>Features</a>
+            </li>
+            <li>
+              <a href='/'>Documentation</a>
+            </li>
+            <li>
+              <a href='/'>Referral Program</a>
+            </li>
+            <li>
+              <a href='/'>Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className='col-span-5'>
+          <h3 className='text-dark-light font-bold'>Product</h3>
+          <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+            <li>
+              <a href='/'>Landingpage</a>
+            </li>
+            <li>
+              <a href='/'>Features</a>
+            </li>
+            <li>
+              <a href='/'>Documentation</a>
+            </li>
+            <li>
+              <a href='/'>Referral Program</a>
+            </li>
+            <li>
+              <a href='/'>Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className='col-span-5'>
+          <h3 className='text-dark-light font-bold'>Product</h3>
+          <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+            <li>
+              <a href='/'>Landingpage</a>
+            </li>
+            <li>
+              <a href='/'>Features</a>
+            </li>
+            <li>
+              <a href='/'>Documentation</a>
+            </li>
+            <li>
+              <a href='/'>Referral Program</a>
+            </li>
+            <li>
+              <a href='/'>Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className='col-span-5'>
+          <h3 className='text-dark-light font-bold'>Product</h3>
+          <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+            <li>
+              <a href='/'>Landingpage</a>
+            </li>
+            <li>
+              <a href='/'>Features</a>
+            </li>
+            <li>
+              <a href='/'>Documentation</a>
+            </li>
+            <li>
+              <a href='/'>Referral Program</a>
+            </li>
+            <li>
+              <a href='/'>Pricing</a>
+            </li>
+          </ul>
+        </div>
+
+      </footer>
+    </section>
   )
 }
 
