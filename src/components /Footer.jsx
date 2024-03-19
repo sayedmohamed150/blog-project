b4cccc25b464da9b341f03be1ff5f74443ee3d17
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-19 06:59:45 +0000

    footer

--- src/components /Footer.jsx
+++ src/components /Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+
+
+import {images} from "../constants";
 
 const Footer = () => {
   return (
@@ -26,62 +31,64 @@ const Footer = () => {
           </ul>
         </div>
         <div className='col-span-5'>
-          <h3 className='text-dark-light font-bold'>Product</h3>
+          <h3 className='text-dark-light font-bold'>Services</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
-            <li>
-              <a href='/'>Landingpage</a>
-            </li>
-            <li>
-              <a href='/'>Features</a>
-            </li>
             <li>
               <a href='/'>Documentation</a>
             </li>
             <li>
-              <a href='/'>Referral Program</a>
+              <a href='/'>Design</a>
             </li>
             <li>
-              <a href='/'>Pricing</a>
+              <a href='/'>Themes</a>
+            </li>
+            <li>
+              <a href='/'>Illustrations</a>
+            </li>
+            <li>
+              <a href='/'>UI Kit</a>
             </li>
           </ul>
         </div>
         <div className='col-span-5'>
-          <h3 className='text-dark-light font-bold'>Product</h3>
+          <h3 className='text-dark-light font-bold'>Company</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
             <li>
-              <a href='/'>Landingpage</a>
+              <a href='/'>About</a>
             </li>
             <li>
-              <a href='/'>Features</a>
+              <a href='/'>Terms</a>
             </li>
             <li>
-              <a href='/'>Documentation</a>
+              <a href='/'>Privacy Policy</a>
             </li>
             <li>
-              <a href='/'>Referral Program</a>
-            </li>
-            <li>
-              <a href='/'>Pricing</a>
+              <a href='/'>Careers</a>
             </li>
           </ul>
         </div>
         <div className='col-span-5'>
-          <h3 className='text-dark-light font-bold'>Product</h3>
+          <h3 className='text-dark-light font-bold'>More</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
-            <li>
-              <a href='/'>Landingpage</a>
-            </li>
-            <li>
-              <a href='/'>Features</a>
-            </li>
             <li>
               <a href='/'>Documentation</a>
             </li>
             <li>
-              <a href='/'>Referral Program</a>
+              <a href='/'>License</a>
             </li>
             <li>
-              <a href='/'>Pricing</a>
+              <a href='/'>Changelog</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img src={images.Logo} alt="logo" />
+          <p>Build a modern and creative website with crealand</p>
+          <ul className='flex justify-center items-center mt-5 space-x-4 text-gray-300'>
+            <li>
+              <a href='/'>
+
+              </a>
             </li>
           </ul>
         </div>
