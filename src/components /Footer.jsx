9f4b898a808e0e520a77bab21f500901314841b0
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-22 11:25:22 +0000

    Footer

--- src/components /Footer.jsx
+++ src/components /Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {AiOutlineTwitter, AiFillYoutube, AiFillInstagram} from "react-icons/ai";
+import {AiOutlineTwitter, AiFillYoutube, AiFillInstagram, AiFillHeart} from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
 import { BsTelegram } from "react-icons/bs";
 
@@ -10,9 +10,9 @@ import {images} from "../constants";
 const Footer = () => {
   return (
     <section className='bg-dark-hard'>
-      <footer className='container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12'>
+      <footer className='container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10'>
 
-        <div className='col-span-5 md:col-span-4 '>
+        <div className='col-span-5 md:col-span-4 lg:col-span-2'>
           <h3 className='text-dark-light font-bold md:text-lg'>Product</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4 md:text-base'>
             <li>
@@ -32,7 +32,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className='col-span-5 md:col-span-4 '>
+        <div className='col-span-5 md:col-span-4 lg:col-span-2'>
           <h3 className='text-dark-light font-bold md:text-lg'>Services</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
             <li>
@@ -52,7 +52,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className='col-span-5 md:col-span-4 '>
+        <div className='col-span-5 md:col-span-4 md:col-start-5 lg:col-span-2 lg:col-start-auto'>
           <h3 className='text-dark-light font-bold md:text-lg'>Company</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
             <li>
@@ -69,7 +69,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className='col-span-5 md:col-span-4 '>
+        <div className='col-span-5 md:col-span-4 lg:col-span-2'>
           <h3 className='text-dark-light font-bold md:text-lg'>More</h3>
           <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
             <li>
@@ -83,9 +83,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className='col-span-10 md:order-first md:col-span-4'>
+        <div className='col-span-10 md:order-first md:col-span-4 lg:col-span-2'>
           <img src={images.Logo} alt="logo" className='brightness-0 invert mx-auto md:mx-0' />
-          <p className='text-sm text-dark-light text-center mt-4 md:text-left md:text-base'>Build a modern and creative website with crealand</p>
+          <p className='text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm'>Build a modern and creative website with crealand</p>
           <ul className='flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start'>
             <li>
               <a href='/'>
@@ -115,7 +115,12 @@ const Footer = () => {
 
           </ul>
         </div>
-
+        <div className='hidden md:flex flex-col items-center space-y-4 md:col-span-12 lg:col-span-10'>
+          <div className='bg-primary text-white p-3 rounded-full'>
+            <AiFillHeart className='w-7 h-auto'/>
+          </div>
+          <p className='font-bold italic text-dark-light '>Copyright © 2019. Moonfo with love.</p>
+        </div>
       </footer>
     </section>
   )
