05b50ba4626e9179308b9a1b74abd106222c910f
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-24 08:34:54 +0000

    Article detail page

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -5,6 +5,7 @@ import { images } from "../../constants";
 import { Link } from "react-router-dom";
 import SuggestedPosts from "./container/SuggestedPosts";
 
+
 const BreadCrumbsData = [
   { name: "Home", link: "/" },
   { name: "Blog", link: "/blog" },
@@ -16,9 +17,40 @@ const postsData = [
       _id: "1",
       image: images.Post1Image,
       title:"Help children get better education",
-      createdAt:""
-    }
+      createdAt: "2023-01-28T15:35:53.607+0000"
+    },
+
+    {
+        _id: "2",
+        image: images.Post1Image,
+        title:"Help children get better education",
+        createdAt: "2023-01-28T15:35:53.607+0000"
+      },
+
+      {
+        _id: "3",
+        image: images.Post1Image,
+        title:"Help children get better education",
+        createdAt: "2023-01-28T15:35:53.607+0000"
+      },
+
+      {
+        _id: "4",
+        image: images.Post1Image,
+        title:"Help children get better education",
+        createdAt: "2023-01-28T15:35:53.607+0000"
+      }
 ]
+
+const tagsData = [
+    "Medical",
+    "Lifestyle",
+    "Learn",
+    "Healthy",
+    "Food",
+    "Diet",
+    "Education",
+];
 
 const ArticleDetailPage = () => {
   return (
@@ -52,7 +84,7 @@ const ArticleDetailPage = () => {
             </p>
           </div>
         </article>
-        <SuggestedPosts header="Latest Article" />
+        <SuggestedPosts header="Latest Article" posts={postsData} tags={tagsData} />
       </section>
     </MainLayout>
   );
--- src/pages/articleDetail/container/SuggestedPosts.jsx
+++ src/pages/articleDetail/container/SuggestedPosts.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import { images } from '../../../constants'
+import { BsTags } from 'react-icons/bs';
 
-const SuggestedPosts = ({ className, header, posts = [] }) => {
+const SuggestedPosts = ({ className, header, posts = [], tags  }) => {
   return (
     <div className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}>
         <h2 className='font-roboto font-medium text-dark-hard'>{header}</h2>
         <div className='grid gap-y-5 mt-5'>
             {posts.map((item) => (
-              <div key={item._id}>
-
+              <div key={item._id} className="flex space-x-3 flex-nowrap items-center">       
+                <img className="aspect-square object-cover rounded-lg w-1/5" src={item.image} alt="laptop" />
+                <div className='text-sm font-roboto text-dark-hard font-medium'>
+                    <h3>{item.title}</h3>
+                    <span className='text-xs opacity-60'>
+                        {new Date(item.createdAt).toLocaleDateString("en-US",{
+                            day: "numeric",
+                            month:"short",
+                            year: "numeric",
+                        })}
+                    </span>
+                </div>
               </div>
             ))}
         </div>
+        <h2 className='flex flex-wrap gap-x-2 gap-y-2 mt-4'>Tags</h2>
+        <div>
+            {tags.map((item) => (
+
+            ))}
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default SuggestedPosts
