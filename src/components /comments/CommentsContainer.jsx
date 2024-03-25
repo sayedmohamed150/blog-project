d136fab8be4c2b188687ad8bbbb70bb2e94cdeda
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-25 08:20:57 +0000

    comments section with replying

---
+++ src/components /comments/CommentsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentsContainer = ({className}) => {
+    return (
+        <div className={`${className}`}>
+
+        </div>
+    )
+}
+
+export default CommentsContainer
