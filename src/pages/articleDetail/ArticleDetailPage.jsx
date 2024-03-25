f17fd0dfc1c0e789566e9b7f22772f0efc04f374
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-25 08:34:25 +0000

    comments section with replying

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,7 @@ import BreadCrumbs from "../../components /BreadCrumbs";
 import { images } from "../../constants";
 import { Link } from "react-router-dom";
 import SuggestedPosts from "./container/SuggestedPosts";
+import CommentsContainer from "../../components /comments/CommentsContainer";
 
 
 const BreadCrumbsData = [
@@ -83,6 +84,7 @@ const ArticleDetailPage = () => {
               lectus proin.
             </p>
           </div>
+          <CommentsContainer className="mt-10"/>
         </article>
         <SuggestedPosts header="Latest Article" posts={postsData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs "/>
       </section>
