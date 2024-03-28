9a80edca46c8ec24c1708f7ed753943161823dc0
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-03-28 06:34:11 +0000

    comments section with replying

--- src/components /comments/CommentsContainer.jsx
+++ src/components /comments/CommentsContainer.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import CommentForm from './CommentForm'
+import React, { useEffect, useState } from "react";
+import { getCommentsData } from "../../data/comments";
 
-const CommentsContainer = ({className}) => {
+import CommentForm from "./CommentForm";
+
+const CommentsContainer = ({ className }) => {
+const [comments, setcomments] = useState([]);
+  
+console.log(comments);
+
+useEffect (() => {
+
+  (async() => {
+    const commentData = await getCommentsData();
+  })()
+}, [])
+
   const addCommentHandler = (value, parent = null, replyOnUser = null) => {
+    const newComment = {
+      _id: "10",
+      user: {
+        _id: "a",
+        name: "Mohammad Rezaii",
+      },
+      desc: "it was a nice post, Thank you!",
+      post: "1",
+      parent: null,
+      replyOnUser: null,
+      createdAt: "2022-12-31T17:22:05.092+0000",
+    };
+  };
 
-  }
+  return (
+    <div className={`${className}`}>
+      <CommentForm
+        btnLabel="Send"
+        formSubmitHandler={(value) => addCommentHandler(value)}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div className={`${className}`}>
-          <CommentForm btnLabel="Send" />
-        </div>
-    )
-}
-
-export default CommentsContainer
+export default CommentsContainer;
