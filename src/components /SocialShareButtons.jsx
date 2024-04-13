53ff815ec5b905de556b49499baaaac82b044c4c
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-04-13 19:23:24 +0000

    Social media share buttons

--- src/components /SocialShareButtons.jsx
+++ src/components /SocialShareButtons.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import { FaFacebookSquare, FaTwitterSquare, FaRedditSquare  } from "react-icons/fa";
-
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaRedditSquare,
+  FaWhatsappSquare,
+} from "react-icons/fa";
 
 const SocialShareButtons = ({ url, title }) => {
   return (
-    <div className='w-full flex justify-between'>
-      <a target='_blank' rel="noreferrer" href='/'>
-        <FaFacebookSquare className='text-[#3b5998] w-12 h-auto'/>
+    <div className="w-full flex justify-between">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://wwww.facebook.com/dialog/share?app_id=1180206992856877&display=popup&href=${url}`}
+      >
+        <FaFacebookSquare className="text-[#3b5998] w-12 h-auto" />
       </a>
-      <a target='_blank' rel="noreferrer" href='/'>
-        <FaTwitterSquare className='text-[#00acee] w-12 h-auto'/>
+      <a target="_blank" rel="noreferrer" href={`https://twitter.com/intent/tweet?url=${url}`}>
+        <FaTwitterSquare className="text-[#00acee] w-12 h-auto" />
       </a>
-      <a target='_blank' rel="noreferrer" href='/'>
-        <FaRedditSquare className='text-[#ff4500] w-12 h-auto'/>
+      <a target="_blank" rel="noreferrer" href={`https://www.reddit.com/submit?url=${url}&title=${title}`}>
+        <FaRedditSquare className="text-[#ff4500] w-12 h-auto" />
+      </a>
+      <a target="_blank" rel="noreferrer" href={`https://api.whatsapp.com/send/?text=${url}`}>
+        <FaWhatsappSquare className="text-[#25D366] w-12 h-auto" />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default SocialShareButtons
+export default SocialShareButtons;
