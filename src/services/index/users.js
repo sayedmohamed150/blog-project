cf3f84022f6ddd2998e5093702a506e47be73514
sayedmohamed150 <154718548+sayedmohamed150@users.noreply.github.com>
2024-05-05 08:32:26 +0000

    UpdateUSERprofile

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -26,7 +26,6 @@ export const login = async ({ email, password }) => {
     throw new Error(error.message);
   }
 };
-
 export const getUserProfile = async ({ token }) => {
   try {
     const config = {
@@ -34,8 +33,28 @@ export const getUserProfile = async ({ token }) => {
         Authorization: `Bearer ${token}`,
       },
     };
-
     const { data } = await axios.get("/api/users/profile", config);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updateProfile = async ({ token, userData }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      "/api/users/updateProfile",
+      userData,
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
